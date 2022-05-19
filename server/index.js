0ea28b30f8afb667ec3a4b7c05d62f954868f192
Riajul Kashem <riajul.programmer@gmail.com>
2022-05-20 01:46:37 +0600

    Audio call socket code added

--- server/index.js
+++ server/index.js
@@ -120,6 +120,35 @@ io.on("connection", (socket) => {
         });
     });
 
+    //Call User
+    socket.on("callUser", ({ userToCall, signalData, from, name }) => {
+        io.to(userToCall).emit("callUser", {
+            signal: signalData,
+            from,
+            name,
+        });
+    });
+
+    socket.on("updateMyMedia", ({ type, currentMediaStatus }) => {
+        console.log("updateMyMedia");
+        socket.broadcast.emit("updateUserMedia", { type, currentMediaStatus });
+    });
+
+    socket.on("msgUser", ({ name, to, msg, sender }) => {
+        io.to(to).emit("msgRcv", { name, msg, sender });
+    });
+
+    socket.on("answerCall", (data) => {
+        socket.broadcast.emit("updateUserMedia", {
+            type: data.type,
+            currentMediaStatus: data.myMediaStatus,
+        });
+        io.to(data.to).emit("callAccepted", data);
+    });
+
+    socket.on("endCall", ({ id }) => {
+        io.to(id).emit("endCall");
+    });
 
     socket.on("disconnect", () => {
         const user = removeUser(socket.id);
